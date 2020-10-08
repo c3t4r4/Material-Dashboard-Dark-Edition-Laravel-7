493fca900e0f6b75c5629a09d49bfe33ed90d123
Glauco Cetara <neocetara@hotmail.com>
2020-10-08 12:53:08 -0300

    Finish Dashboard Files

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,48 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+    .sass('resources/views/MaterialDashboardDark/assets/scss/material-dashboard.scss', 'public/MaterialDashboardDark/css/material-dashboard.css')
+
+    .styles([
+        'resources/views/MaterialDashboardDark/assets/css/font-awesome.min.css'
+    ], 'public/MaterialDashboardDark/css/font-awesome.min.css')
+
+    .styles([
+        'resources/views/MaterialDashboardDark/assets/demo/demo.css'
+    ], 'public/MaterialDashboardDark/demo/demo.css')
+
+    .scripts([
+        'resources/views/MaterialDashboardDark/assets/js/core/jquery.min.js',
+        'resources/views/MaterialDashboardDark/assets/js/core/popper.min.js',
+        'resources/views/MaterialDashboardDark/assets/js/core/bootstrap-material-design.min.js',
+        'resources/views/MaterialDashboardDark/assets/js/default-passive-events.js',
+        'resources/views/MaterialDashboardDark/assets/js/plugins/perfect-scrollbar.jquery.min.js',
+        'resources/views/MaterialDashboardDark/assets/js/buttons.js'
+    ], 'public/MaterialDashboardDark/js/material-core.js')
+
+    .scripts([
+        'resources/views/MaterialDashboardDark/assets/js/plugins/chartist.min.js'
+    ], 'public/MaterialDashboardDark/js/plugins/chartist.min.js')
+
+    .scripts([
+        'resources/views/MaterialDashboardDark/assets/js/plugins/bootstrap-notify.js'
+    ], 'public/MaterialDashboardDark/js/plugins/bootstrap-notify.js')
+
+    .scripts([
+        'resources/views/MaterialDashboardDark/assets/js/material-dashboard.js'
+    ], 'public/MaterialDashboardDark/js/material-dashboard.js')
+
+    .scripts([
+        'resources/views/MaterialDashboardDark/assets/demo/demo.js'
+    ], 'public/MaterialDashboardDark/demo/demo.js')
+
+
+
+    .copyDirectory('resources/views/MaterialDashboardDark/assets/img','public/MaterialDashboardDark/img')
+    .copyDirectory('resources/views/MaterialDashboardDark/assets/fonts','public/MaterialDashboardDark/fonts')
+
+    .options({
+        processCssUrls: false
+    })
+    .version();
