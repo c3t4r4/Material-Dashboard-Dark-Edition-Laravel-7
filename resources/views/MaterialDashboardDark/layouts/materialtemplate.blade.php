6e87e8f7e72a0ab98693398d2d86f3915014cc59
Glauco Cetara <neocetara@hotmail.com>
2020-10-08 12:47:59 -0300

    Create a Template Default

---
+++ resources/views/MaterialDashboardDark/layouts/materialtemplate.blade.php
@@ -0,0 +1,25 @@
+<!--
+=========================================================
+* Material Dashboard Dark Edition - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-dark
+* Copyright 2019 Creative Tim (http://www.creative-tim.com)
+
+* Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+    @hasSection('header')
+        @yield('header')
+    @endif
+
+    @hasSection('body')
+        @yield('body')
+    @endif
+</html>
