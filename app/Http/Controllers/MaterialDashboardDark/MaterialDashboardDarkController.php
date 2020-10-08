cdd4df552995cb656f289cfca31f5d7dc1af58bf
Glauco Cetara <neocetara@hotmail.com>
2020-10-08 12:48:25 -0300

    Create Controller and Dashboard Method

---
+++ app/Http/Controllers/MaterialDashboardDark/MaterialDashboardDarkController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\MaterialDashboardDark;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MaterialDashboardDarkController extends Controller
+{
+    public function dashboard()
+    {
+        return view('MaterialDashboardDark.pages.dashboard');
+    }
+}
